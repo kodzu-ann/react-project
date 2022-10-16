a057698f36a17b4e11a27d0f798f705d8c2cd25c
Kodzu-ann <ann.cooper2023@gmail.com>
2022-10-16 20:11:54 +0200

    link is added

--- src/App.js
+++ src/App.js
@@ -4,8 +4,18 @@ import SearchEngine from "./SearchEngine";
 
 export default function App() {
   return (
-    <div className="App">
-      <SearchEngine />
-    </div>
+    <>
+      <div className="App">
+        <SearchEngine />
+      </div>
+      <a
+        href="https://github.com/kodzu-ann/react-project.git"
+        className="github-link"
+        target="_blank"
+        rel="noreferrer"
+      >
+        link to github
+      </a>
+    </>
   );
 }
